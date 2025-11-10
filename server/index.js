4dfe29ddfba6288802199121f32062af2f5ca8ec
kaveesha wijegurusinghe <155706266+kaveesha4890@users.noreply.github.com>
2025-11-10 18:33:39 +0530

    import middlewares

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 dotenv.config();
 connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 const port = process.env.port || 3001;
 
