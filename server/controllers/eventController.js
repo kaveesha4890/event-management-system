126d5547914cc166c3e8a97765ea4c0c0a46acd5
kaveesha wijegurusinghe <155706266+kaveesha4890@users.noreply.github.com>
2025-11-15 23:20:17 +0530

    develop controllers for events

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -29,3 +29,81 @@ export const  createEvent = async(req,res) => {
         res.status(500).json({message: "Internal server error"})
     }
 }
+
+//Get all Events
+export const getAllEvents = async(req,res) => {
+    try {
+        const events = await Event.find().populate("organizer", "name email role").sort({createdAt: -1});
+
+        return res.status(200).json(events);
+    } catch (error) {
+        console.error("Get all Event Error: ", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+//Get single event
+export const getEventById = async(req,res) => {
+    try {
+        const event = await Event.findById(req.params.id).populate("organizer", "name email role");
+
+        if(!event){
+            return res.status(404).json({message: "Event not found"});
+        }
+
+        return res.status(200).json(event);
+    } catch (error) {
+        console.error("Get all Event Error: ", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+//Update Event
+export const UpdateEvent = async(req,res) => {
+    try {
+        const event = await Event.findById(req.params.id)
+
+        if(!event){
+            return res.status(404).json({message: "Event not found"});
+        }
+
+        if(event.organizer.toString() !== user._id.toString()){
+            return res.status(403).json({message: 'Not allowed'});
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+
+        return res.status(200).json({
+            message:"Event updated successfully",
+            event: updatedEvent,
+        });
+    } catch (error) {
+        console.error("Get all Event Error: ", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+//Delete Event
+export const deleteEvent = async(req,res) => {
+    try {
+        const event = await Event.findById(req.params.id)
+
+        if(!event){
+            return res.status(404).json({message: "Event not found"});
+        }
+
+         if(event.organizer.toString() !== user._id.toString()){
+            return res.status(403).json({message: 'Not allowed'});
+        }
+        await event.deleteOne();
+
+        return res.status(200).json({message: "Event deleted successfully"});
+    } catch (error) {
+        console.error("Get all Event Error: ", error);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createEvent } from '../controllers/eventController.js';
+import { createEvent, getAllEvents } from '../controllers/eventController.js';
 import { authMiddleware, roleMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post("/create", authMiddleware,roleMiddleware("organizer"), createEvent)
+router.get("/all-events", getAllEvents)
 
 export default router;
