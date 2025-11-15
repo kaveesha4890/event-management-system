e084c0573966c68f8357e80c79ace20f86f4a649
kaveesha wijegurusinghe <155706266+kaveesha4890@users.noreply.github.com>
2025-11-15 07:53:00 +0530

    develop login api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,7 +3,6 @@ import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken'
 
 //User signup
-
 export const registerUser = async(req,res)=> {
     try {
         const {name, email, password, role} = req.body;
@@ -40,7 +39,56 @@ export const registerUser = async(req,res)=> {
             },
         });
     } catch (error) {
-        console.error("Register Error: ",error);
+        console.error("Register Error: ", error);
         return res.status(500).json({message:"Internal server error"})
+    }
+}
+
+//user login
+export const loginUser = async(req,res) => {
+    try {
+        const {email, password} = req.body;
+
+        //check fields
+        if(!email || !password){
+            return res.status(400).json({message:"Email and password required"});
+        }
+
+        //find user by email
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+
+        //compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message:"Invalid password"})
+        }
+
+        //create JWT token
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role:user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        );
+        return res.status(200).json({
+            message:"Login successful",
+            token,
+            user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            },
+        });
+    } catch (error) {
+        console.error("Login Error: ", error);
+        res.status(500).json({message:"Internal server error"})
     }
 }
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { registerUser } from '../controllers/authController.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const userRoute = express.Router();
 
 userRoute.post("/register", registerUser);
+userRoute.post("/login", loginUser)
 
 export default userRoute;
